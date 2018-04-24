7125ff933ab05b428561febfc1df787648c29387
Zach <zach@kobalt-orange.com>
2018-04-24 11:26:39 -0500

    removed pubsub
    and reworked implementaion of idea to save time

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,12 @@
+$(document).ready(function(){
+    $(function(){
+        // $('#get-channels').click(function() {
+        //     main();
+        // });
+        main();
+    });
+});
+
 var channelList = [];
 
 function main() {
@@ -16,56 +25,3 @@ function main() {
         console.log(channelList);
     });
 }
-
-function pubSub() {
-    requirejs(["ext/request"], function(request) {
-        channelList.forEach(function (value) {
-            request.post('https://pubsubhubbub.appspot.com/subscribe', {
-                form: {
-                    'hub.mode': ('subscribe'),
-                    'hub.callback': 'https://webhook.site/96f9eeef-41d5-4b81-beed-ba921e84048f',
-                    'hub.topic': 'https://www.youtube.com/xml/feeds/videos.xml?channel_id=' + channelList[value].id,
-                    'hub.lease_seconds': '31536000'
-                }
-            }, (error, pubSubResponse) => {
-                if (!error && pubSubResponse.statusCode == 202) {
-                    res.status(200).json({
-                        code: 'channel_subscribed',
-                        details: "Subscribed/unsubscribed succesfully to https://www.youtube.com/channel/" + channelList[value].id
-                    })
-                } else {
-                    res.status(500).json({
-                        code: 'subscription_failed',
-                        details: "An error occured while connecting to Google's PubSubHubbub Hub",
-                        error
-                    })
-                }
-            })
-        });
-    });
-}
-
-// create a function to subscribe to topics
-// var mySubscriber = function (msg, data) {
-//     console.log( msg, data );
-// };
-//
-// // add the function to the list of subscribers for a particular topic
-// // we're keeping the returned token, in order to be able to unsubscribe
-// // from the topic later on
-// var token = PubSub.subscribe('MY TOPIC', mySubscriber);
-//
-// // publish a topic asyncronously
-// PubSub.publish('MY TOPIC', 'hello world!');
-//
-// // publish a topic syncronously, which is faster in some environments,
-// // but will get confusing when one topic triggers new topics in the
-// // same execution chain
-// // USE WITH CAUTION, HERE BE DRAGONS!!!
-// PubSub.publishSync('MY TOPIC', 'hello world!');
-
-// function pubSub() {
-//     var request = require('request');
-//
-
-// }
